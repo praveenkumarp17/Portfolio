279c44da7d8050d66ee3d00e8cf5ac2701595eab
praveenkumarp17 <113337116+praveenkumarp17@users.noreply.github.com>
2024-08-05 20:04:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+/*==============Typed code================*/
+
+
+var typed = new Typed(".auto-type",{
+    strings:["Fullstack Developer","Web Developer", "Data Analyed"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true,
+});
